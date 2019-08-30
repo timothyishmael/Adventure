78c550c57f351b07d6aa7d737d91baeac039c4b5
timothyishmael <timothyishmael1@yahoo.com>
2019-08-30 17:46:37 -0400

    added more options

--- Actors/Characters/AttackOptions/AllAttackOptions.h
+++ Actors/Characters/AttackOptions/AllAttackOptions.h
@@ -2,5 +2,8 @@
 #define ALL_ATTACK_OPTIONS_H
 #include"AttackOption.h"
 #include"NakedAttackOption.h"
-
+#include"OneHandedAttackOption.h"
+#include"TwoHandedAttackOption.h"
+#include"SwordAndShieldOption.h"
+#include"PureShieldOption.h"
 #endif
